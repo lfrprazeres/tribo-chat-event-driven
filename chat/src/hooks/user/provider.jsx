9b9e687e78d6be160e7c34e0b7a481dfcc3f57d9
Luiz Fernando <lfrprazeres@gmail.com>
2024-10-29 20:35:57 -0300

    1.1 - código inicial

---
+++ chat/src/hooks/user/provider.jsx
@@ -0,0 +1,31 @@
+import { useEffect, useState } from 'react';
+import { userContext } from './context';
+import useChat from 'hooks/chat';
+import fetch from 'config/fetchInstance';
+
+export const UserProvider = ({ children }) => {
+  const [data, setData] = useState(null);
+  const { data: chatData } = useChat();
+
+  useEffect(() => {
+    const currentChatIndex = data?.chats.findIndex(chat => chat.id === chatData?.id);
+    if (data?.chats[currentChatIndex]?.unreadMessages > 0) {
+      setData(oldData => ({
+        ...oldData,
+        chats: oldData.chats.map(chat => {
+          if (chat.id === chatData?.id) {
+            return { ...chat, unreadMessages: 0 }
+          }
+          return chat;
+        })
+      }))
+      fetch.post(`/api/chats/${chatData?.id}/readMessages`, { id: data.id });
+    }
+  }, [data?.id, data?.chats, chatData?.id])
+
+  return (
+    <userContext.Provider value={{ data, setData }}>
+      {children}
+    </userContext.Provider>
+  );
+};
