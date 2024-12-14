f1b7da6255373e31e00ace66ae0b2b3e9148009f
Luiz Fernando <lfrprazeres@gmail.com>
2024-12-14 18:38:27 -0300

    3.4 - Conversando com joão

--- server/routes/users.js
+++ server/routes/users.js
@@ -7,11 +7,13 @@ function getUsuarioById(id, cb) {
     const { users, chats } = newDb;
     const userIndex = users.findIndex((userDb) => userDb.id === Number(id));
     const user = { ...users[userIndex] };
+    let nextChatId = chats[chats.length - 1].id + 1;
 
     if (user) {
       const otherUsers = [...users.filter((userDb) => userDb.id !== user.id)];
 
       otherUsers?.map((userDb) => {
+        const otherUserIndex = users.findIndex(user => user.id === userDb.id);
         const userChatIndex = user.chats?.findIndex((chatDb) =>
           chatDb.participants?.includes(userDb.id),
         );
@@ -31,6 +33,7 @@ function getUsuarioById(id, cb) {
           participants: [user.id, userDb.id],
           type: 'chat',
           unreadMessages: 0,
+          id: nextChatId,
           ...userChat,
           ...completeChat,
         };
@@ -39,6 +42,20 @@ function getUsuarioById(id, cb) {
           user.chats[userChatIndex] = newChat;
         } else {
           user.chats.push(newChat);
+          chats.push({
+            id: nextChatId,
+            type: 'chat',
+            participants: [user.id, userDb.id],
+            messages: []
+          });
+          users[otherUserIndex].chats.push({
+            name: user.name,
+            image: user.image,
+            participants: [user.id, userDb.id],
+            unreadMessages: 0,
+            type: 'chat',
+            id: nextChatId
+          })
         }
       });
 
